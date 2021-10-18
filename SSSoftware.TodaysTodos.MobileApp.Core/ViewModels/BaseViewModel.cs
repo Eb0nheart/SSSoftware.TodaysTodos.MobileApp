3deeb7f0af321e2a0eb0c22567a62e4b8eda3f3d
Eb0nheart <76154207+Eb0nheart@users.noreply.github.com>
2021-10-18 18:23:37 +0200

    Setup for Async data loading when view appears.

---
+++ SSSoftware.TodaysTodos.MobileApp.Core/ViewModels/BaseViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace SSSoftware.TodaysTodos.MobileApp.Core.ViewModels
+{
+    public abstract class BaseViewModel
+    {
+        public abstract Task InitializeAsync();
+
+        public ICommand LoadDataCommand
+            => new Command(() => InitializeAsync());
+    }
+}
--- SSSoftware.TodaysTodos.MobileApp.Core/ViewModels/TodoViewModel.cs
+++ SSSoftware.TodaysTodos.MobileApp.Core/ViewModels/TodoViewModel.cs
@@ -3,20 +3,37 @@ using SSSoftware.TodaysTodos.MobileApp.Core.Models;
 
 namespace SSSoftware.TodaysTodos.MobileApp.Core.ViewModels
 {
-    public class TodoViewModel 
+    public class TodoViewModel : BaseViewModel
     {
+        private const string GetUrl = "http://192.168.87.132:3000/todo";
+        private readonly HttpClient httpClient;
+
         public ObservableCollection<Todo> Todos { get; set; }
 
-        public TodoViewModel()
+        public TodoViewModel(HttpClient httpClient)
         {
-            Todos = new ObservableCollection<Todo>
-            {
-                new Todo
-                {
-                    Id = 1,
-                    Description = "Clean apartment"
-                }
-            };
+            Todos = new ObservableCollection<Todo>();
+            this.httpClient = httpClient;
         }
-    }
+
+        public override async Task InitializeAsync()
+        {
+            try
+            {
+                var todos = await httpClient.GetFromJsonAsync<IEnumerable<Todo>>(GetUrl);
+                foreach (var todo in todos)
+                {
+                    Todos.Add(todo);
+                }
+            }
+            catch (HttpRequestException)
+            {
+                // TODO: Configure logging and log error here.
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    } 
 }
